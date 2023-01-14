374ea3525559d67b8340148cb2594914a7538d54
Theia <100139847+ththeia@users.noreply.github.com>
2023-01-14 21:56:30 +0200

    Server update

--- feedback-app/server/server.js
+++ feedback-app/server/server.js
@@ -6,92 +6,271 @@ const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'database.db',
   define: {
-    timestamps: false
+    timestamps: true
   }
 })
 
 // Entity definitions
 
+const Activity = sequelize.define('activity', {
+  accessCode: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+  },
+  description: Sequelize.STRING,
+  date: Sequelize.DATE,
+  validUntil: Sequelize.DATE,
+})
+
+const Feedback = sequelize.define('feedback', {
+  reaction: Sequelize.ENUM("smile", "frown", "surprised", "confused"),
+},
+{
+  timestamps: true,
+  createdAt: true,
+  updatedAt: false, // feedback entries cannot be updated, so we do not need this field.
+  defaultScope: {
+    attributes: { exclude: ['activityAccessCode'] },
+  }
+})
+
 const User = sequelize.define('user', {
   firstName: Sequelize.STRING,
   lastName: Sequelize.STRING,
-  username: Sequelize.STRING,
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
   password: Sequelize.STRING,
-  role: Sequelize.STRING,
+  role: {
+    type: Sequelize.ENUM("student", "professor"),
+    defaultValue: "student",
+  },
+}, {
+  defaultScope: {
+    attributes: { exclude: ['password'] },
+  }
 })
+
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-// User endpoints
+/// Activity endpoints ///
 
+// Creates activity.
+app.post('/activities', async (req, res) => {
+  try {
+    const newActivity = await Activity.create(req.body)
+    res.status(201).json(newActivity)
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: e })
+  }
+})
+
+// Gets all activities.
+app.get('/activities', async (req, res) => {
+  try {
+    const activities = await Activity.findAll()
+    res.status(200).json(activities)
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: e })
+  }
+})
+
+// Gets activity by code.
+app.get('/activities/:code', async (req, res) => {
+  try {
+    const activity = await Activity.findByPk(req.params.code)
+    if (activity) {
+      res.status(200).json(activity)
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: e })
+  }
+})
+
+// Updates (edits) an activity.
+app.put('/activities/:code', async (req, res) => {
+  try {
+    const activity = await Activity.findByPk(req.params.code)
+    if (activity) {
+      await activity.update(req.body)
+      res.status(202).json({ message: 'accepted' })
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: e })
+  }
+})
+
+// Deletes an activity.
+app.delete('/activities/:code', async (req, res) => {
+  try {
+    const activity = await Activity.findByPk(req.params.code)
+    if (activity) {
+      await activity.destroy()
+      res.status(202).json({ message: 'accepted' })
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: e })
+  }
+})
+
+// Creates a new feedback instance for a given activity.
+app.post('/activities/:code/feedback', async (req, res) => {
+  try {
+    const activity = await Activity.findByPk(req.params.code)
+    if (activity) {
+      const reaction = req.body.reaction;
+
+      const newFeedback = await Feedback.create({
+        activityCode: req.params.code,
+        reaction: reaction,
+      })
+
+      res.status(201).json(newFeedback)
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: e })
+  }
+})
+
+// Get feedback for a given activity.
+app.get('/activities/:code/feedback', async (req, res) => {
+  try {
+    const activity = await Activity.findByPk(req.params.code)
+    if (activity) {
+
+      const feedback = await Feedback.findAll({
+        where: {
+          activityCode: req.params.code
+        }
+      });
+
+      res.status(200).json(feedback)
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }
+  } catch (e) {
+    console.warn(e)
+    res.status(500).json({ message: e })
+  }
+})
+
+
+
+/// User endpoints ///
+
+// Creates a new user.
 app.post('/users', async (req, res) => {
   try {
     await User.create(req.body)
     res.status(201).json({ message: 'created' })
   } catch (e) {
     console.warn(e)
-    res.status(500).json({ message: 'server error' })
+    res.status(500).json({ message: e })
   }
 })
 
+// Gets user by username.
 app.get('/users/:username', async (req, res) => {
   try {
-    const found = false;
-    const users = await User.findAll();
-    for(var u of users) {
-      if(u.username === req.params.username) {
-        found = true;
-        break;
+    const user = await User.findOne({
+      where: {
+        username: req.params.username
       }
-    }
-    if (found) {
-      res.status(200).json(found)
+    })
+
+    if (user) {
+      res.status(200).json(user)
     } else {
       res.status(404).json({ message: 'not found' })
     }
   } catch (e) {
     console.warn(e)
-    res.status(500).json({ message: 'server error' })
+    res.status(500).json({ message: e })
   }
 })
 
-
+// Log in.
 app.post('/login', async (req, res) => {
   try {
-    var username = req.body.username
-    var password = req.body.password
-
-    const users = await User.findAll();
-    for(var u of users) {
-      if(u.username === username) {
-        if(u.password === password) {
-          res.status(200).json({ firstName: u.firstName, lastName: u.lastName, username: u.username, role: u.role })
-        } else {
-          res.status(400).json({ message: 'Incorrect user or password' })
-        }
-        break;
+    const user = await User.findOne({
+      where: {
+        username: req.body.username,
+        password: req.body.password
       }
+    })
+
+    if (user) {
+      res.status(200).json({ firstName: user.firstName, lastName: user.lastName, username: user.username, role: user.role })
+    } else {
+      res.status(404).json({ message: 'Incorrect user or password' })
     }
   } catch (e) {
     console.warn(e)
-    res.status(500).json({ message: 'server error' })
+    res.status(500).json({ message: e })
   }
 })
 
+
+// Initialize server.
 app.listen(8080, async () => {
   try {
+    // Table associations
+    Activity.hasMany(Feedback, {
+      onDelete: 'CASCADE'
+    });
+    Feedback.belongsTo(Activity, {
+      foreignKey: 'activityCode',
+      allowNull: false
+    });
+
     await sequelize.sync({ alter: true })
     console.warn('created tables')
-    const teacher = User.build({ firstName: "El", lastName: "Professor", username: "user1", password: "user1", role: "professor" });
-    const student = User.build({ firstName: "Bob", lastName: "Stamina", username: "user2", password: "user2", role: "student" });
-    const sample = [teacher, student]
-    sample.forEach(async (u) => {
-      console.warn('creating ' + u.username)
-      //await User.create(e)
-      u.save()
+
+    await User.findOrCreate({
+      where: {
+        firstName: "El", lastName: "Professor", username: "user1", password: "user1", role: "professor"
+      }
     })
+
+    await User.findOrCreate({
+      where: {
+        firstName: "Bob", lastName: "Stamina", username: "user2", password: "user2", role: "student"
+      }
+    })
+
+    /*const prof = User.build({ firstName: "El", lastName: "Professor", username: "user1", password: "user1", role: "professor" });
+    const student = User.build({ firstName: "Bob", lastName: "Stamina", username: "user2", password: "user2", role: "student" });
+    const sample = [prof, student]
+    sample.forEach(async (u) => {
+      console.info('creating ' + u.username)
+      try {
+        u.save();
+      }
+      catch (e) {
+        console.error(e);
+      }
+    })
+    */
   } catch (err) {
     console.warn(err)
   }
